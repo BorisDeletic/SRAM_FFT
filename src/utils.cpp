3971db8589972645ec02637aad0d1cbd9cce5869
Boris Deletic <boris.deletic@monash.edu>
2020-04-24 21:58:37 +1000

    reordered

--- src/utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "config/config.h"
-#include "internal/vptr_utils.h"
-
-namespace virtmem {
-
-const NILL_t NILL = NILL_t(); // need empty constructor to prevent compiler error on arduino
-
-}
